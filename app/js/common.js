70c29226b29b041dd155c9b46910ebf959bab3d1
Juan <sandovaljuan427@gmail.com>
2018-10-07 22:29:11 -0700

    Initial Commit

--- app/js/common.js
+++ app/js/common.js
@@ -2,4 +2,55 @@ $(function() {
 
 	// Custom JS
 
+
+
+
+
+
+
 });
+
+
+
+$('.button').mousedown(function() {
+	$(this).addClass('down');
+});
+
+
+$('.button').mouseup(function() {
+	$(this).removeClass('down');
+});
+
+
+function viewProjects(type) {
+
+	if(type == 'developer') { var othertype = 'designer'; }
+	if(type == 'designer') { var othertype = 'developer'; }
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById(type).innerHTML = this.responseText;
+		}
+	};
+	xhttp.open("GET", type + "-projects.html", true);
+	xhttp.send();
+
+	var xhttp2 = new XMLHttpRequest();
+	xhttp2.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById(othertype).innerHTML = this.responseText;
+		}
+	};
+	xhttp2.open("GET", othertype + "-skills.html", true);
+	xhttp2.send();
+
+	if ($(window).width() > 750) {
+		$('#' + type).addClass('scroll');
+	$('#' + othertype).removeClass('scroll');
+	 }
+
+	
+
+}
+
